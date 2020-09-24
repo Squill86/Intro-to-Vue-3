377dacb5f28173db76c8f24f4d1ae8ac83fcab58
Jack Watts <jack.watts@trenchdigital.net>
2020-09-24 09:59:46 +0100

    Lesson 04 code challenge

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 25,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['small', 'medium', 'large'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
